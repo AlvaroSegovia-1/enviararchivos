424799180dfd6223556068fc009953a51d2d6718
Alvagit25 <alvarodesegovia@gmail.com>
2022-01-07 10:34:38 +0100

    crear schema

---
+++ NodeSend-Servidor/controllers/usuarioController.js
@@ -0,0 +1,19 @@
+const Usuario = require("../models/Usuario");
+
+exports.nuevoUsuario = async (req, res) => {
+  //console.log("Desde nuevo Usuario");
+  //console.log(req.body);
+
+  // Verificar si el usuario ya estuvo registrado
+  const { email } = req.body;
+  let usuario = await Usuario.findOne({ email });
+
+  if (usuario){
+      return res.status(400).json({msg:'El usuario ya está registrado'})
+  }
+
+  usuario = await new Usuario(req.body);
+  usuario.save();
+
+  res.json({ msg: "Usuario Creado Correctamente" });
+};
