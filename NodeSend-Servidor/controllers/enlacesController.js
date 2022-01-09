5a0acb8fa086b91ac8f5dcbffef0b657f380932c
Alvagit25 <alvarodesegovia@gmail.com>
2022-01-09 11:08:24 +0100

    crear enlaces

--- NodeSend-Servidor/controllers/enlacesController.js
+++ NodeSend-Servidor/controllers/enlacesController.js
@@ -1,7 +1,58 @@
-exports.nuevoEnlace = async (req, res, next) =>{
-    //console.log('desde nuevo enlace')
+const Enlaces = require("../models/Enlace");
+const shortid = require("shortid");
+const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
 
-    // Revisar si hay errores
+exports.nuevoEnlace = async (req, res, next) => {
+  //console.log('desde nuevo enlace')
 
-    // Almacenar en la BD
-}
+  // Revisar si hay errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+
+  // Crear un objeto de enlace
+  //console.log(req.body);
+
+  const { nombre_original } = req.body;
+
+  const enlace = new Enlaces();
+  enlace.url = shortid.generate();
+  enlace.nombre = shortid.generate();
+  enlace.nombre_original = nombre_original;
+
+  //console.log(enlace);
+
+  // Si el usuario está autenticado
+  //console.log(req.usuario)
+
+  if (req.usuario) {
+    const { password, descargas } = req.body;
+    enlace.password = password;
+
+    // Asignar a enlace el número de descargas
+    if (descargas) {
+      enlace.descargas = descargas;
+    }
+
+    // Asignar un password
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+
+      enlace.password = await bcrypt.hash(password, salt);
+    }
+
+    // Asignar el autor
+    enlace.autor = req.usuario.id;
+  }
+
+  // Almacenar en la BD
+  try {
+    await enlace.save();
+    return res.json({ msg: `${enlace.url}` });
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
