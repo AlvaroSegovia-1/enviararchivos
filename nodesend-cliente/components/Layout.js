28f97bbef1805e32278e86b17b87df34703352cd
Alvagit25 <alvarodesegovia@gmail.com>
2022-01-11 12:17:59 +0100

    iniciar nextjs

---
+++ nodesend-cliente/components/Layout.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import { Header } from "./Header";
+
+export const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Enviar archivos</title>
+        <script src='https://cdn.tailwindcss.com'></script>
+      </Head>
+
+      {/* <img src='logo.svg' /> */}
+      <div className='bg-gray-100 min-h-screen'>
+        <div className='container mx-auto px-40'>
+          <Header />
+          <main className='mt-20'>{children}</main>
+        </div>
+      </div>
+    </>
+  );
+};
